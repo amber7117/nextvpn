632e0eb5ac2d0858474b80c1ce6f1361f4b3925a
amber7117 <kenny@kennys-MacBook-Pro.local>
2024-06-18 19:45:15 +0800

    ok

--- .next/static/yYvJdFipjuhYaYWUsJQsy/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/268-630d4c1a542f9553.js","static/chunks/pages/index-514da267a77578e3.js"],"/_error":["static/chunks/pages/_error-3c9a37b9888d2d4e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
